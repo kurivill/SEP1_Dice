4d7237e7aaaee47bcde25e977d26a4c0752df03a
kurivill <kurikkaville@hotmail.fi>
2025-02-26 11:39:23 +0200

    Testing

---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiceTest {
+
+    @org.junit.jupiter.api.Test
+    void roll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkDouble() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
